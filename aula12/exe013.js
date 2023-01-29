f03519d75275c0d8e089ee627dd5213ca51eda68
Ranieri Oliveira <114862419+ranierioliveira@users.noreply.github.com>
2023-01-29 20:50:07 -0300

    Aprendendo condições múltiplas

---
+++ aula12/exe013.js
@@ -0,0 +1,27 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+
+switch (diaSem) {
+    case 0:
+        console.log('Domingo')
+        break
+    case 1: 
+        console.log('Segunda')
+        break
+    case 2: 
+        console.log('Terça')
+        break
+    case 3:
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('Quinta')
+        break
+    case 5:
+        console.log('Sexta')
+        break
+    case 6:
+        console.log('Sábado')
+        break 
+}
+/* O uso do switch é mais pontual, apenas para números e strings, não senso possível o uso em intervalos*/
