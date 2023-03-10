4038e19e5813ff1e597abb7a562789b86db3fe2d
Ranieri Oliveira <114862419+ranierioliveira@users.noreply.github.com>
2023-03-10 18:45:36 -0300

    Estudos de estruturas de repetição for e do while

---
+++ aula13/ambiente.js
@@ -0,0 +1,29 @@
+let c = 1;
+
+do{
+    console.log("Tudo bem? nº " + c);
+    c++;
+} while(c <= 6);
+
+
+
+
+
+/*while(c <= 3){
+    console.log("Tudo bem? nº " + c);  
+    c ++;
+}*/
+
+
+
+
+
+
+
+
+
+/*console.log("Tudo bem?");
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+console.log("Tudo bem?");*/
