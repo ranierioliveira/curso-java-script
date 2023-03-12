3396d6e13a10635039f151bcd0120f14a913336c
Ranieri Oliveira <114862419+ranierioliveira@users.noreply.github.com>
2023-03-12 13:59:21 -0300

    Adc. verificações no contador

--- contador/script.js
+++ contador/script.js
@@ -1,5 +1,20 @@
 function contar(){
-    let inicio = Number(document.getElementById("inicio").value);
-    let fim = Number(document.getElementById("fim").value)
-    
+  let inicio = Number(document.getElementById("inicio").value);
+  let fim = Number(document.getElementById("fim").value);
+  let passo = Number(document.getElementById("passos").value);
+  let res = document.getElementById("res");
+  res.innerHTML = ""
+  
+  if (passo <= 0){
+    alert("Passo inválido, considerando passo 1");
+    passo = 1;
   }
+  if(inicio == "" || fim == "" || passo == "" || inicio > fim){
+    res.innerHTML = "Impossível contar!"
+  }else{
+    for (inicio; inicio <= fim; inicio = inicio + passo){
+      res.innerHTML +=  " " + inicio;
+    }
+  }
+  
+}
