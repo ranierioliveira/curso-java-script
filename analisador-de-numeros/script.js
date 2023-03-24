236daf4d16f964169fb17fb50623e5d36a902733
Ranieri Oliveira <114862419+ranierioliveira@users.noreply.github.com>
2023-03-24 08:43:30 -0300

    Outro modo de se fazer um mesmo exercício

---
+++ analisador-de-numeros/script.js
@@ -0,0 +1,27 @@
+let num = document.querySelector("input#fnum");
+let lista = document.querySelector("select#flista");
+let res = document.querySelector("p#res")
+let valores = [];
+
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true;
+    } else {
+        return false;
+    }
+}
+function inLista(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function adicionar(){
+    if(isNumero(num.value) &&  !inLista(num.value, valores)){
+        alert("Tudo certo!");
+    } else {
+        alert("Número inválido ou já encontrado na lista.");
+    }
+}
--- ultimo-exercicio/script.js
+++ ultimo-exercicio/script.js
@@ -2,23 +2,50 @@ function adicionar(){
     let numeroAdicionado = document.getElementById("inum");
     let mostrador = document.getElementById("iselecionados");
 
+    
     if(numeroAdicionado.value.length == 0){
         alert("Digite algum número para ser adicionado!")
     }else{
         let valor = Number(numeroAdicionado.value);
         if(valor >= 1 && valor <= 100){
-            guardaValores.push(valor);
+            //confere a posição do valor na lista, se não -1 não existe ainda
+            if(guardaValores.indexOf(valor) === -1){
+                guardaValores.push(valor);
 
-            let item = document.createElement('option');
-            item.text += `valor ${valor} foi adicionado`;
-            mostrador.appendChild(item); 
+                let item = document.createElement('option');
+                item.text += `valor ${valor} foi adicionado`;
+                mostrador.appendChild(item); 
+            } else{
+                alert("Valor já encontrado na lista!")
+            }
         }else{
-            alert("Digite um valor entre 0 e 100");
+            alert("Digite um valor entre 0 e 100!");
         } 
     }  
+    numeroAdicionado.value= "";
     }
+    
 let guardaValores = [];
 
 function finalizar(){
-    console.log(guardaValores);
+
+    if(guardaValores.length == 0){
+        alert("Adicione valores antes de finalizar!")
+    }else{
+        let soma = 0;
+    
+        for(let pos in guardaValores){
+            soma += guardaValores[pos];
+        }
+        let media = soma/guardaValores.length
+    
+        let res = document.getElementById("resultados");
+        res.innerHTML = `Ao todo temos ${guardaValores.length} números cadastrados.<br>`
+        res.innerHTML += `O maior número informado foi ${Math.max.apply(null, guardaValores)}.<br>`;
+        res.innerHTML += `O menor número informado foi ${Math.min.apply(null, guardaValores)}.<br>`;
+        res.innerHTML += `A soma dos valores é de ${soma}.<br>`;
+        res.innerHTML += `A média dos valores é de ${media.toFixed(2)}.`; 
+    }
+
 }
+//Math.max.apply(null, meuArray)
